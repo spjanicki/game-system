3290f41b35a0a01d3035ba905d5fb57eddf1567c
Sarah Portaria <sarah.portaria@gmail.com>
2022-08-16 19:53:00 -0400

    Added pwd hash + verify if user is unic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,9 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { Player } from './player.entity';
 
+// TODO : Should create a 'PlayerResume' so i can hide the pwd
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
@@ -9,5 +11,15 @@ export class AuthController {
   @Post('/register')
   register(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.authService.register(authCredentialsDto);
+  }
+
+  @Get()
+  getAllPlayers(): Promise<Player[]> {
+    return this.authService.getAllPlayers();
+  }
+
+  @Get('/:id')
+  getPlayerById(@Param('id') id: string): Promise<Player> {
+    return this.authService.getPlayerById(id);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,12 +2,19 @@ import {
   ConflictException,
   Injectable,
   InternalServerErrorException,
+  NotFoundException,
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { PlayerLeague } from './player-league.enum';
 import { Player } from './player.entity';
+import * as bcrypt from 'bcrypt';
+import { NotFoundError } from 'rxjs';
+
+enum PlayerCreationError {
+  ALREADY_EXIST = '23505',
+}
 
 @Injectable()
 export class AuthService {
@@ -19,22 +26,46 @@ export class AuthService {
   async cratePlayer(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
 
+    const salt = await bcrypt.genSalt();
+    const hashedPwd = await bcrypt.hash(password, salt);
+
     const creationDate = new Date();
 
-    // Add validations
     const player = this.playerRepository.create({
       username,
-      password,
+      password: hashedPwd,
       rating: 0,
       currentLeague: PlayerLeague.BEGINNER,
       isLoggedIn: true,
       creationDate,
     });
 
-    await this.playerRepository.save(player);
+    try {
+      await this.playerRepository.save(player);
+    } catch (error) {
+      console.log(error);
+      if (error.code === PlayerCreationError.ALREADY_EXIST) {
+        throw new ConflictException('Username already exist');
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
   }
 
   async register(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.cratePlayer(authCredentialsDto);
+  }
+
+  async getAllPlayers(): Promise<Player[]> {
+    return await this.playerRepository.find();
+  }
+
+  async getPlayerById(id: string): Promise<Player> {
+    const player = await this.playerRepository.findOne({ where: { id } });
+
+    if (!player) {
+      throw new NotFoundException(`No player with ID ${id} exists.`);
+    }
+    return player;
   }
 }
